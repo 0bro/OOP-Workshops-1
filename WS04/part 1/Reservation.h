6136fe1da3c2f31d9b2c1f2a04e5e2965d8d7d60
0bro <65639158+0bro@users.noreply.github.com>
2020-10-16 22:10:49 -0400

    Part 1

---
+++ WS04/part 1/Reservation.h
@@ -0,0 +1,26 @@
+#ifndef SDDS_RESERVATION_H
+#define SDDS_RESERVATION_H
+
+#include <iostream>
+#include <string>
+namespace sdds
+{
+    const size_t ID_MAX = 8;
+    class Reservation
+    {
+        std::string m_reserveID;
+        std::string m_reserveName;
+        std::string m_email;
+        size_t m_partySize;
+        size_t m_day;
+        size_t m_hour;
+
+    public:
+        Reservation();
+        Reservation(const std::string &res);
+        friend std::ostream &operator<<(std::ostream &os, Reservation out);
+    };
+
+} // namespace sdds
+
+#endif
